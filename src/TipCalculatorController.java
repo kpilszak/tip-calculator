98f27a9d561b8bc4b135b9e183057f0cb5390300
kpilszak <klaudia.pilszak@gmail.com>
2019-05-30 16:58:33 +0200

    Create TipCalculatorController.java

---
+++ src/TipCalculatorController.java
@@ -0,0 +1,28 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class TipCalculatorController {
+
+    @FXML
+    private Label tipPercentageLabel;
+
+    @FXML
+    private TextField amountTextField;
+
+    @FXML
+    private TextField tipTextField;
+
+    @FXML
+    private TextField totalTextField;
+
+    @FXML
+    private Slider tipPercentageSlider;
+
+    @FXML
+    void calculateButtonPressed(ActionEvent event) {
+
+    }
+
+}
